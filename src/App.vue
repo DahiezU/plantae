3f0110e376cf2f1e9c4e136df4a844555389988b
SimonBurdy <simon.burdy@lacatholille.fr>
2021-04-11 14:51:51 +0200

    map orépationnelle

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ import PanneauHeader from './components/PanneauHeader.vue'
 import Profil from './components/user/Profil.vue'
 import Presentation from './components/home/Presentation.vue';
 import Map from './components/marketMap/Map.vue';
+import axios from 'axios';
 
 export default defineComponent({
     name: 'App',
--- src/main.ts
+++ src/main.ts
@@ -2,34 +2,33 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+
+
 
 import PrimeVue from 'primevue/config'
-/*import Button from 'primevue/button'
-import InputText from 'primevue/inputtext'
-import Toast from 'primevue/toast'
-import ToastService from 'primevue/toastservice';
 
-import Checkbox from 'primevue/checkbox';
-
-*/
 import 'primevue/resources/themes/saga-blue/theme.css'       //theme
 import 'primevue/resources/primevue.min.css'                 //core css
 import 'primeicons/primeicons.css'                           //icons
 
 import TabMenu from 'primevue/tabmenu';
 
+
+
+
+
+
 const app = createApp(App);
+
+
 app.use(PrimeVue);
-//app.use(ToastService);
-
-
-
-/*app.component('InputText', InputText);
-app.component('Button', Button);
-app.component('Toast', Toast);
-
-app.component('Checkbox',Checkbox);*/
+app.use(VueAxios , axios);
 
 app.component('TabMenu',TabMenu);
+
+
 app.use(router).mount('#app')
 
