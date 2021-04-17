c5bf3e5ba74be4f1f234be45f69ec0c739993bf1
SimonBurdy <simon.burdy@lacatholille.fr>
2021-04-17 22:19:44 +0200

    afficher res recherche quasi ok

--- src/components/myMarket/itemsMarket.vue
+++ src/components/myMarket/itemsMarket.vue
@@ -2,22 +2,49 @@
     <div>
         <div>
             <h5>Basic</h5>
-            <AutoComplete v-model="selectedItem" :suggestions="filteredItems" @complete="searchItem($event)" field="name" />
+            <AutoComplete v-model="selectedItem" :suggestions="filteredItems" @complete="searchItem($event)" field="name"  placeholder="Search"/>
+            <Button icon="pi pi-check" label="Search" @click="loadItems()"/>
+        </div>
 
+        <div v-if="afficherRes">
+          <tr v-for="item in itemsFound.value.hints" :key="item.food.foodId">
+            <Card>
+              <template #header>
+                  <img id="imagProduct"   alt="img product" :src="item.food.image">
+                  
+              </template>
+              <template #title>
+                {{item.food.label}}
+              </template>
+              <template #content>
+              
+                category: {{item.food.category}} <br>
+              
+          
+              </template>
+              <template #footer>
+                  <Button icon="pi pi-check" label="Save" />
+                  <Button icon="pi pi-times" label="Cancel" class="p-button-secondary" style="margin-left: .5em" />
+              </template>
+            </Card> 
+          </tr>
         </div>
     </div>
 </template>
 
 <script>
 import ItemService from './itemsService';
+import axios from 'axios'
 import {FilterService,FilterMatchMode} from 'primevue/api';
-
+const itemsFound= {};
 export default {
     data() {
         return {
             items: null,
             selectedItem: null,
             filteredItems: null,
+            afficherRes:false,
+            itemsFound
           
           
         }
@@ -71,6 +98,27 @@ export default {
                 }
             }, 250);
         },
-    }
+      loadItems() {
+            //console.log(this.selectedItem.name);
+            this.afficherRes = true;
+            axios
+              .get( 'https://api.edamam.com/api/food-database/v2/parser?ingr='+this.selectedItem.name+'&app_id=21137dee&app_key=3ec1733a6d09062c59d4ef9451d12035')
+              .then(function (response) {
+                //console.log(response)
+                itemsFound.value = response.data
+              })
+              .catch(function (error) {
+                console.error(error);
+              });
+            //console.log(this.items.value)
+              console.log(itemsFound)
+
+              return{
+                itemsFound
+              }
+          }
+    },
+    
+
 }
 </script>
